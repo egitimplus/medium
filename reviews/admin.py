ec158f75520062f3b2a1f9c3ce9459bab1377c4c
Emre Çevik <info@internet.com.tr>
2020-10-13 18:35:05 +0300

    Part 1

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Product, Category, Company, ProductSize, ProductSite, Comment
+from django.contrib.auth.models import Group
 
-# Register your models here.
+
+# admin.register() decorator
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'content', )
+    list_filter = ('category', )
+
+#admin.site.register(Product, ProductAdmin) <- admin.site.register method
+
+admin.site.register(Category)
+admin.site.register(Company)
+admin.site.register(ProductSize)
+admin.site.register(ProductSite)
+admin.site.register(Comment)
+
+admin.site.unregister(Group)
+
+admin.site.site_header = "Product Review Admin"
